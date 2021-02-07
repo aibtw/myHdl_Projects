04ddc32bd2c8f54b208f6a4aea169058d8962bfe
HeshamTB <hishaminv@gmail.com>
2021-02-07 22:54:49 +0300

    4-bit decoder done

--- Decoder-4bit/Decoder-4bit.py
+++ Decoder-4bit/Decoder-4bit.py
@@ -2,44 +2,45 @@ from myhdl import always, block, instance, \
     always_comb, Signal, intbv, delay, \
     traceSignals, Simulation, toVerilog
 
-@block
+
 def decoder_4bit(inpu, out, enable):
 
-    @always(inpu, enable.posedge)
+    @always_comb
     def logic():
         if enable:
-            out[inpu].next = 1
+            if inpu == 0:
+                out.next = 0
+            else:
+                out.next = 2 ** inpu
+        else:
+            out.next = 0
 
     return logic
 
-@block
+
 def test_decoder():
     inp = Signal(intbv(0)[4:])
-    en = Signal(bool(0))
+    en = Signal(bool(1))
     out = Signal(intbv(0)[16:])
     dec = decoder_4bit(inp, out, en)
 
     @always(delay(10))
     def stim():
-        if inp < 16:
+        if inp < 15:
             inp.next = inp + 1
         else:
             inp.next = inp
 
-    @always(delay(25))
-    def toggle():
-        en.next = not en
-
-    return stim, toggle, dec
+    return stim, dec
 
 
 def simulate(timesteps):
-    #tb = traceSignals(test_decoder)
-    #sim = Simulation(tb)
-    #sim.run(timesteps)
-    tb = test_decoder()
-    tb.config_sim(trace=True)
-    tb.run_sim(1000)
+    tb = traceSignals(test_decoder)
+    sim = Simulation(tb)
+    sim.run(timesteps)
+    # tb = test_decoder()
+    # tb.config_sim(trace=True)
+    # tb.run_sim(1000)
 
 
 def convertToVer():
