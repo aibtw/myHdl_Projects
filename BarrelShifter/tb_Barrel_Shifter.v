e0f7f3a1529369573b48162fb002f814934c4849
Aib <ah444mad@hotmail.com>
2021-02-10 10:50:08 +0300

    Redesigned barrel shifter

--- BarrelShifter/tb_Barrel_Shifter.v
+++ BarrelShifter/tb_Barrel_Shifter.v
@@ -3,15 +3,11 @@ module tb_Barrel_Shifter;
 reg [11:0] par_in;
 wire [11:0] par_out;
 reg [2:0] shift_amount;
-reg shift;
-reg load;
 
 initial begin
     $from_myhdl(
         par_in,
-        shift_amount,
-        shift,
-        load
+        shift_amount
     );
     $to_myhdl(
         par_out
@@ -21,9 +17,7 @@ end
 Barrel_Shifter dut(
     par_in,
     par_out,
-    shift_amount,
-    shift,
-    load
+    shift_amount
 );
 
 endmodule
